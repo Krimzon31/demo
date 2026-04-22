cebeeb8f814633a271abaa7e33adb048a5980b91
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2026-04-22 18:34:41 +0300

    Add files via upload

---
+++ Demo/ConnectionClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using SD = System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class ConnectionClass
+    {
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=Krimzon; Initial Catalog=demo; Integrated Security=True");
+
+        public ConnectionClass()
+        {
+
+        }
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == SD.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == SD.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
